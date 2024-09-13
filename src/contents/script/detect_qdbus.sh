cc1d7f12213ce8ce4672e12813e4fe0cf66d4c1a
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2024-09-13 20:45:43 +0200

    Fix usage of arrays in detect_qdbus.sh
    POSIX compliant shells, like the Ubuntu's default dash, don't support
    arrays.

--- src/contents/script/detect_qdbus.sh
+++ src/contents/script/detect_qdbus.sh
@@ -3,13 +3,12 @@
 # A list of possible qdbus names
 # Fedora uses qdbus-qt6
 # OpenSUSE uses qdbus6
-qdbus_names=(
-qdbus-qt6
-qdbus6
-qdbus
-)
+qdbus_names="\
+qdbus-qt6 \
+qdbus6 \
+qdbus"
 
-for name in ${qdbus_names[@]}; do
+for name in ${qdbus_names}; do
     $name > /dev/null
     if test $? -eq 0; then
         qdbus_name=$name
